26cba25d54d169fc05508bbb4a764eb1f118954c
Ben Miller <bmiller@rackspace.com>
2015-07-31 20:40:33 -0400

    post-rebase cleanup

--- spec/models/role_spec.rb
+++ spec/models/role_spec.rb
@@ -3,13 +3,13 @@ require 'rails_helper'
 RSpec.describe Role, type: :model do
   let(:valid_attributes) do
     {
-      name: "visitor"
+      name: "Visitor"
     }
   end
 
   it "belongs_to user_type" do
     role = Role.create! valid_attributes
-    User.create!(name: "Bob", role: Role.find_by(name: "visitor"))
+    User.create!(name: "Bob", role: Role.find_by(name: "Visitor"))
     expect(role.users).to eq [User.first]
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,12 +4,12 @@ RSpec.describe User, type: :model do
   let(:valid_attributes) do
     {
       name: "Bob User",
-      role: Role.find_by(name: "visitor")
+      role: Role.find_by(name: "Visitor")
     }
   end
 
   it "belongs_to user_type" do
-    Role.create! name: "visitor"
+    Role.create! name: "Visitor"
     user = User.create! valid_attributes
     expect(user.role).to eq Role.first
   end
