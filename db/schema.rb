ff5bc22f18a768f42ed685c92cb85c0a93473ed5
Ben Miller <bmiller@rackspace.com>
2015-07-31 23:43:30 -0400

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -12,11 +12,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20150801020109) do
-  create_table "roles", force: :cascade do |t|
-    t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
   create_table "activities", force: :cascade do |t|
     t.string   "name"
