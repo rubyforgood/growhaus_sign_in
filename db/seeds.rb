678072775a8a522d3321fa23c0ab416d681b0912
Matt Morrisey <mattydevicce@gmail.com>
2015-08-01 12:23:06 -0400

    Finished the seeds for roles and activities to be idempotent

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,30 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 # Create roles
-Role.create name: "Volunteer"
-Role.create name: "Visitor"
-Role.create name: "Intern"
+roles = ["Volunteer", "Visitor", "Intern"]
+roles = roles.map do |t| 
+  Role.find_or_create_by(name: t)
+end
+
+activities_volunteer = ["Food Box Sorting", "Cosechando Salud", "Hydroponics Farm",
+              "Aquaponics Farm", "Growasis", "Mushroom Farm", "Education",
+              "Marketing/Communications", "Spanish Translation", "Construction/Facilities",
+              "Graphic Design", "Special Event", "Other"]
+
+activities_intern = ["Hydroponics Farm", "Aquaponics Farm", "Executive Assistant Intern",
+                     "Communication Intern", "Digital Design Intern", "Service Learning Intern",
+                     "Community Education Intern", "Food Distribution Intern", "Development Intern"]
+
+
+activities_volunteer.each do |a|
+  activity = Activity.find_or_create_by(name: a)
+  activity.role << roles[0] unless activity.role.include? roles[0]
+end
+
+activities_intern.each do |a|
+  activity = Activity.find_or_create_by(name: a)
+  activity.role << roles[2] unless activity.role.include? roles[2]
+end
+
+
+
