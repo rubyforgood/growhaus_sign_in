5efbd22392f329abd6a7d5e2edab7d98ec328836
Ben Miller <bmiller@rackspace.com>
2015-08-01 14:49:24 -0400

    satisfy waiver requirement in model specs

--- spec/models/role_spec.rb
+++ spec/models/role_spec.rb
@@ -9,7 +9,9 @@ RSpec.describe Role, type: :model do
 
   it "belongs_to user_type" do
     role = Role.create! valid_attributes
-    User.create!(name: "Bob", role: Role.find_by(name: "Visitor"))
+    User.create!(name: "Bob",
+                 role: Role.find_by(name: "Visitor"),
+                 waiver_signature: "Bob/s/")
     expect(role.users).to eq [User.first]
   end
 end
