84b18b1a3ec40258e244536a4d04173c1a5bbca1
Robert Winter <rlwinter@gmail.com>
2015-08-01 16:13:04 -0400

    first working version

--- app/models/staff.rb
+++ app/models/staff.rb
@@ -2,15 +2,12 @@ class Staff < ActiveRecord::Base
   def self.find_or_create_from_auth(data)
     staff = Staff.find_or_create_by(provider: data.provider, uid: data.uid)
 
-    puts data.awesome_inspect
-    Rails.logger.info data.awesome_inspect 
-
     staff.name = data.info.name
     staff.email = data.info.email
     staff.image_url = data.info.image
-    #staff.account_url = data.info.urls.Google
+    staff.account_url = data.info.urls.Google
     staff.provider = data.provider
-    #staff.token = data.credentials.token
+    staff.token = data.credentials.token
     staff.uid = data.uid
     staff.save
 
--- spec/auth_helper.rb
+++ spec/auth_helper.rb
@@ -6,13 +6,14 @@ module OmniAuthMacros
     # The mock_auth configuration allows you to set per-provider (or default)
     # authentication hashes to return during integration testing.
     #OmniAuth.config.mock_auth[:google] = OmniAuth::AuthHash.new({
-    OmniAuth.config.add_mock(:google, {
-      'provider' => 'Google',
+    OmniAuth.config.add_mock(:google_oauth2, {
+      'provider' => 'google_oauth2',
       'uid' => '123545',
       'info' => {
         'name' => 'mockuser',
         'email' => 'mockuser@growhaus.org',
-        'image' => 'mock_user_thumbnail_url'
+        'image' => 'mock_user_thumbnail_url',
+        'urls' => {'Google': 'https://plus.google.com'}
       },
       'credentials' => {
         'token' => 'mock_token',
@@ -29,3 +30,39 @@ end
 
 OmniAuth.config.test_mode = true
 
+#{
+#       "provider" => "google_oauth2",
+#            "uid" => "",
+#           "info" => {
+#              "name" => "Robert Winter",
+#             "email" => "",
+#        "first_name" => "Robert",
+#         "last_name" => "Winter",
+#             "image" => "https://lh3.googleusercontent.com/photo.jpg?sz=50",
+#              "urls" => {
+#            "Google" => "https://plus.google.com/1"
+#        }
+#    },
+#    "credentials" => {
+#             "token" => "xxx",
+#        "expires_at" => 1438462534,
+#           "expires" => true
+#    },
+#          "extra" => {
+#        "id_token" => "xxxx",
+#        "raw_info" => {
+#                      "kind" => "plus#personOpenIdConnect",
+#                    "gender" => "male",
+#                       "sub" => "1",
+#                      "name" => "Robert Winter",
+#                "given_name" => "Robert",
+#               "family_name" => "Winter",
+#                   "profile" => "https://plus.google.com/1",
+#                   "picture" => "https://lh3.googleusercontent.com/photo.jpg?sz=50",
+#                     "email" => "",
+#            "email_verified" => "true",
+#                    "locale" => "en"
+#        }
+#    }
+#}
+#
