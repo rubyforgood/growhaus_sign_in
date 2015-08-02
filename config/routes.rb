7ff8d2b26a91f01dac16edb107c6fc515f3a0e79
Ben Miller <bmiller@rackspace.com>
2015-08-01 21:11:38 -0400

    add role selection page

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
   resources :staffs
 
-  scope '(:locale)', :locale => /en|es/ do
+  scope '(:locale)', locale: /en|es/ do
     get 'login' => 'pages#login'
     get 'welcome' => 'pages#welcome'
+
     resources :users do
       resources :activity_sessions, only: :index
       resources :activities, only: :index
     end
+    get 'users/new_role' => 'users#new_role'
+
     resources :activity_sessions, only: %i[ create update ]
   end
 
